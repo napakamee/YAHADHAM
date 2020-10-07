9a6b39411aca0e830b03ad06d1f2fed677100fe4
DESKTOP-0S0KTE1\FzBth <Jintaphum_sun@cmu.ac.th>
2020-10-07 19:15:06 +0700

    Add Background parallax effect

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [Space(2)]
+    [Header("Nebula Sprites")]
+    [SerializeField] GameObject nebulaSprite1;
+    [SerializeField] GameObject nebulaSprite2;
+    [SerializeField] Vector3 nebulaRespawnPoint;
+    [SerializeField] float nebulaSpriteSpeed;
+    
+   [Space(2)]
+
+    [Header("Planet Sprites")]
+    [SerializeField] GameObject[] PlanetSprites;
+    [SerializeField] float PlanetsSpawner;
+    [SerializeField] float PlanetsRespawnPoint;
+    [SerializeField] float PlanetsSpriteSpeed;
+
+    private void Update()
+    {
+        nebulaSprite1.transform.position += new Vector3(Time.deltaTime * -nebulaSpriteSpeed, 0, 0);
+        nebulaSprite2.transform.position += new Vector3(Time.deltaTime * -nebulaSpriteSpeed, 0, 0);
+        for (int i = 0; i < PlanetSprites.Length; i++)
+        {
+            PlanetSprites[i].transform.position += new Vector3(Time.deltaTime * -PlanetsSpriteSpeed, 0, 0);
+        }
+    }
+    private void LateUpdate()
+    {
+
+        if (nebulaSprite1.transform.position.x < nebulaRespawnPoint.x)
+        {
+            nebulaSprite1.transform.position = new Vector3(nebulaSprite2.transform.position.x + nebulaSprite2.GetComponent<Collider2D>().bounds.size.x, 0, 0);
+        }
+        if (nebulaSprite2.transform.position.x < nebulaRespawnPoint.x)
+        {
+            nebulaSprite2.transform.position = new Vector3(nebulaSprite1.transform.position.x + nebulaSprite1.GetComponent<Collider2D>().bounds.size.x, 0, 0);
+        }
+        for (int i = 0; i < PlanetSprites.Length; i++)
+        {
+            if (PlanetSprites[i].transform.position.x < PlanetsRespawnPoint)
+            {
+                PlanetSprites[i].transform.position = new Vector3(PlanetsSpawner, PlanetSprites[i].transform.position.y, PlanetSprites[i].transform.position.z);
+            }
+        }
+    }
+}
