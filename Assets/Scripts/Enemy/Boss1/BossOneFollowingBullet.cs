6f96de1d393fd33b2a4f1c68c229060bbe2b0e6e
DESKTOP-325D3AA\Nustsuto <weenut_s@cmu.ac.th>
2020-10-27 16:12:26 +0700

    Boss Stage 1 Push with no sprite

---
+++ Assets/Scripts/Enemy/Boss1/BossOneFollowingBullet.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossOneFollowingBullet : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+    public float fireRate;
+    float nextFire;
+    public int ammoAmount;
+    float Interval = 1;
+    public float delay = 1;
+    bool LaserOn;
+    public GameObject Laser;
+    bool IsStop;
+    public float timeStart = 8;
+    Rigidbody2D rbb;
+
+    public float speed = 3;
+    void Start()
+    {
+        rbb = GetComponent<Rigidbody2D>();
+
+        rbb.velocity = new Vector2(-speed, 0);
+        IsStop = false;
+        ammoAmount = 4;
+        fireRate = 1.0f;
+        nextFire = Time.time;
+        LaserOn = false;
+        Laser.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        Interval -= Time.deltaTime;
+        CheckToFire();
+        if (ammoAmount >= 1 && Interval <= 0)
+        {
+            shot();
+            ammoAmount--;
+            Interval = 1;
+        }
+        if(transform.position.x <= 6.82f && !IsStop)
+        {
+            rbb.velocity = new Vector2(0,-1);
+            IsStop = true;
+        }
+        if(transform.position.y >= 2.3f){
+            rbb.velocity = new Vector2(0,-1);
+        }
+        if(transform.position.y <= -1.58f)
+        {
+            rbb.velocity = new Vector2(0,1);
+        }
+
+    }
+
+    void CheckToFire()
+    {
+
+        if (ammoAmount <= 0)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                LaserOn = true;
+                Laser.SetActive(true);
+                Debug.Log("LaserOn");
+                TimerLaser();
+                //delay=2;
+            }
+        }
+    }
+
+    void shot()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+    void TimerLaser()
+    {
+        timeStart -= Time.deltaTime;
+        if (timeStart <= 1)
+        {
+            Debug.Log("LaserOff");
+            ammoAmount = 4;
+            LaserOn = false;
+            Laser.SetActive(false);
+            timeStart = 8;
+            delay = 2;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Boss1/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed =5f;
+
+    Rigidbody2D rb;
+
+    [SerializeField]
+    GameObject target;
+ //   int ammoAmount;
+
+ //   bool LaserOn;
+  //  public float timeStart = 8;
+ //   public GameObject Laser;
+    
+
+    Vector2 moveDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        moveDir = (target.transform.position - transform.position).normalized * speed;
+        rb.velocity = new Vector2(moveDir.x, moveDir.y);
+      //  Destroy (gameObject,5f);
+     //   ammoAmount =4;
+        //LaserOn = false;
+    }
+
+   
+   
+void OnTriggerEnter2D(Collider2D col)
+{
+    if(col.gameObject.tag.Equals("Player"))
+    {
+            Destroy (gameObject);
+    }
+}
+}
